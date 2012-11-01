9bac8900fc039fd07b319b66da59a93e7a363a90
Soeren Kroell <kroell@idseven.de>
2012-11-01 13:16:02 +0100

    Uebungsaufgabe ROT13

--- ueb1/rot13/rot13.c
+++ ueb1/rot13/rot13.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/* BERECHNUNG DER VERSCHLUESSELUNG A-Z*/
+int calcA(int character)
+{
+  int sum;
+  return sum = ((((character-'A')+13) % 26)+'A');
+}
+
+/* BERECHNUNG DER VERSCHLUESSELUNG a-z*/
+int calca(int character)
+{
+  int sum;
+  return sum = ((((character-'a')+13) % 26)+'a');
+}
+
 
 int main(void)
 {
-
-
-
+   int c;
+ 
+   while ((c = getchar()) != EOF) 
+   {
+     if (c >= 'A' && c <= 'Z')
+     {
+       c = calcA(c); 
+     }
+     else if (c >= 'a' && c <= 'z')
+     {
+       c = calca(c);
+     }
+     putchar(c);     
+   }
+   return 0;
 }
+
+
+
